c6489953f28cf44c593efc15c634961ac463066e
Abolfazl Panahiazar <abolfazlpanahiazar@gmail.com>
2020-08-04 12:56:34 +0430

    Add layout components header, sidebar, footer

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import React from "react";
-import { Container } from "reactstrap";
+import { Container, Row, Col } from "reactstrap";
 import { Switch, Route } from "react-router-dom";
 import routes from "./routes.js";
+
+// Components
+import Header from "./Components/Header/Header";
+import SideBar from "./Components/SideBar/SideBar";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
   return (
     <Container bg="primary">
-      <Switch>
-        {routes.map((route) => (
-          <Route {...route} />
-        ))}
-      </Switch>
+      <Header />
+      <Row className="d-flex">
+        <Col xs="12" md="8">
+          <Switch>
+            {routes.map((route) => (
+              <Route {...route} />
+            ))}
+          </Switch>
+        </Col>
+        <Col xs="12" md="4">
+          <SideBar />
+        </Col>
+      </Row>
+      <Footer />
     </Container>
   );
 }
