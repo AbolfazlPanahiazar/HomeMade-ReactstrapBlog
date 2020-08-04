796acd509b93d6fe84b21c34d0ac3d06ccc39366
Abolfazl Panahiazar <abolfazlpanahiazar@gmail.com>
2020-08-04 18:41:43 +0430

    Add SideBar and render photos in it

--- src/Components/SideBar/SideBar.jsx
+++ src/Components/SideBar/SideBar.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Card, CardImg, CardBody, CardTitle } from "reactstrap";
 
 function SideBar() {
-  return <div>ساید بار</div>;
+  const [photos, setPhotos] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.ir/photos")
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response);
+        setPhotos(response.filter((item, index) => index < 3));
+      });
+  }, []);
+
+  return (
+    <aside>
+      {photos.map((photo) => (
+        <Card key={photo.id} className="my-2">
+          <CardImg top width="100%" src={`https://picsum.photos/id/${photo.id * 53}/600/300`} alt="Card image cap" />
+          <CardBody className="text-center">
+            <CardTitle>{photo.title}</CardTitle>
+          </CardBody>
+        </Card>
+      ))}
+    </aside>
+  );
 }
 
 export default SideBar;
