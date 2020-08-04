83518c5ee9a3ba81d8e61c0b019cc110bc5d367c
Abolfazl Panahiazar <abolfazlpanahiazar@gmail.com>
2020-08-04 20:58:40 +0430

    Add Single User page

--- src/Pages/PostSingle/PostSingle.jsx
+++ src/Pages/PostSingle/PostSingle.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button } from "reactstrap";
+import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle } from "reactstrap";
 
 function PostSingle() {
   const { postId } = useParams({});
--- src/Pages/UserSingle/UserSingle.jsx
+++ src/Pages/UserSingle/UserSingle.jsx
@@ -1,7 +1,55 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams, Link } from "react-router-dom";
+import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button } from "reactstrap";
 
 function UserSingle() {
-  return <div>تک کاربر</div>;
+  const { userId } = useParams({});
+
+  const [user, setUser] = useState({});
+
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.ir/users/${userId}`)
+      .then((response) => response.json())
+      .then((response) => {
+        setUser(response);
+      });
+    fetch(`https://jsonplaceholder.ir/posts?userId=${userId}`)
+      .then((res) => res.json())
+      .then((res) => {
+        setPosts(res);
+      });
+  }, []);
+
+  return (
+    <main>
+      <Card className="mt-2">
+        <CardImg top width="100%" src={user.avatar} alt="Card image cap" />
+        <CardBody>
+          <CardTitle className="text-right">نام و نام خانوادگی: {user.name}</CardTitle>
+          <CardSubtitle className="text-right">{user.username} :نام کاربری</CardSubtitle>
+          <CardSubtitle className="text-right">{user.email} :پست الکترونیک</CardSubtitle>
+          <CardSubtitle className="text-right">{user.phone} :شماره تماس</CardSubtitle>
+          <CardSubtitle className="text-right">
+            <a href={user.website}>{user.website}</a> :وبسایت
+          </CardSubtitle>
+        </CardBody>
+      </Card>
+      <h4 className="mt-3 text-right">پست ها منتشر شده توسط این کاربر</h4>
+      {posts.map((post) => (
+        <Card key={post.id} dir="rtl" className="text-right my-2 bg-dark text-light">
+          <CardBody>
+            <CardTitle>{post.title}</CardTitle>
+            <CardSubtitle>{post.body}</CardSubtitle>
+            <Link className="mt-2" to={`/blog/post/${post.id}`}>
+              <Button>مشاهده ی پست</Button>
+            </Link>
+          </CardBody>
+        </Card>
+      ))}
+    </main>
+  );
 }
 
 export default UserSingle;
