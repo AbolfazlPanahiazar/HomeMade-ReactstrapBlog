ce0bd54499f8e9b0e04222a276a74f8710c13ef7
Abolfazl Panahiazar <abolfazlpanahiazar@gmail.com>
2020-08-04 19:20:31 +0430

    Add Posts page

--- src/Pages/Posts/Posts.jsx
+++ src/Pages/Posts/Posts.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Row, Col, Card, CardImg, CardBody, CardTitle, CardSubtitle } from "reactstrap";
+import { Link } from "react-router-dom";
 
 function Posts() {
-  return <div>همه پست ها</div>;
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.ir/posts")
+      .then((response) => response.json())
+      .then((response) => {
+        setPosts(response);
+      });
+  }, []);
+
+  return (
+    <main>
+      <Row xs={1} lg={2}>
+        {posts.map((post) => (
+          <Col>
+            <Link className="text-light" to={`/blog/post/${post.id}`}>
+              <Card style={{ minHeight: "450px" }} className="my-3">
+                <CardImg top width="100%" src={`https://picsum.photos/id/${post.id + 3}/600/400`} alt="Card image cap" />
+                <CardBody className="text-right bg-dark text-light">
+                  <CardTitle>{post.title}</CardTitle>
+                  <CardSubtitle>{post.body}</CardSubtitle>
+                </CardBody>
+              </Card>
+            </Link>
+          </Col>
+        ))}
+      </Row>
+    </main>
+  );
 }
 
 export default Posts;
