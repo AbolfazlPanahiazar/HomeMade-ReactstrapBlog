16cbb0484972a29a5fbdd83b100a28e46a297902
Abolfazl Panahiazar <abolfazlpanahiazar@gmail.com>
2020-08-04 20:34:37 +0430

    Add PostSingle page

--- src/Pages/PostSingle/PostSingle.jsx
+++ src/Pages/PostSingle/PostSingle.jsx
@@ -1,7 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button } from "reactstrap";
 
 function PostSingle() {
-  return <div>تک پست</div>;
+  const { postId } = useParams({});
+
+  const [post, setPost] = useState({
+    title: "",
+    body: "",
+  });
+
+  const [auther, setAuther] = useState({});
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.ir/posts/${postId}`)
+      .then((response) => response.json())
+      .then((response) => {
+        setPost(response);
+        fetch(`https://jsonplaceholder.ir/users/${response.userId}`)
+          .then((res) => res.json())
+          .then((res) => {
+            setAuther(res);
+          });
+      });
+  }, []);
+
+  return (
+    <div>
+      <Card className="mt-2">
+        <CardImg top width="100%" src={`https://picsum.photos/id/${post.id + 3}/600/400`} alt="Card image cap" />
+        <CardBody className="text-right">
+          <CardTitle>{post.title}</CardTitle>
+          <CardSubtitle>تاریخ انتشار: دوازده مرداد ۱۳۹۹</CardSubtitle>
+          <CardText>{post.body.repeat(30)}</CardText>
+        </CardBody>
+      </Card>
+      <Card className="mt-1 bg-dark text-light">
+        <div dir="rtl" className="d-flex justify-content-between">
+          <img src={auther.avatar} alt="auther" />
+          <div className="d-flex flex-column mx-2 text-center justify-content-center">
+            <span>{auther.name}</span>
+            <span>{auther.email}</span>
+            <span>{auther.phone}</span>
+          </div>
+        </div>
+      </Card>
+    </div>
+  );
 }
 
 export default PostSingle;
